93d291f4a7887196fd3368cd2c6c976cf0df52e5
CaioCosta29 <caiocostacb@gmail.com>
2026-08-18 12:51:09 -0300

    Commit inicial

---
+++ src/main/java/com/caio/ci_lab/CiLabApplication.java
@@ -0,0 +1,13 @@
+package com.caio.ci_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/ci_lab/CiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.ci_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
